e9de384a21aeee255246fa8e0c98160c5e44df66
bluwolfer777 <leonleonleonr@gmail.com>
2024-03-18 11:33:27 +0100

    added owlcarousel lib

--- Script/main.js
+++ Script/main.js
@@ -1,10 +1,38 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
 function loadXML() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("test").innerHTML = xhttp.responseText;
+            writeXML(xhttp);
         }
     }
     xhttp.open("GET", "Asset/catalog.xml", true);
     xhttp.send();
+}
+
+function writeXML(xml) {
+    var x, i, xmlDoc, txt;
+    xmlDoc = xml.responseXML;
+    txt = "";
+    x = xmlDoc.getElementsByTagName("ARTIST");
+    for (i = 0; i< x.length; i++) {
+        txt += x[i].childNodes[0].nodeValue + "<br>";
+    }
+    document.getElementById("test").innerHTML = txt;
 }
