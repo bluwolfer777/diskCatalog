9c4c819a1deca75e36ffbae69f9cbd79425b7fc3
bluwolfer777 <leonleonleonr@gmail.com>
2024-04-29 11:32:46 +0200

    added search

--- Script/main.js
+++ Script/main.js
@@ -1,19 +1,14 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+
+function filter(title, artist) {
+    search = document.getElementById("search").value.toLowerCase();
+    if (title.toLowerCase().indexOf(search) > -1 || artist.toLowerCase().indexOf(search) > -1) {
+        return true
     }
-})
+    else {
+        return false
+    }
+}
+
 
 function loadXML() {
     var xhttp = new XMLHttpRequest();
@@ -29,10 +24,25 @@ function loadXML() {
 function writeXML(xml) {
     var x, i, xmlDoc, txt;
     xmlDoc = xml.responseXML;
-    txt = "";
-    x = xmlDoc.getElementsByTagName("ARTIST");
-    for (i = 0; i< x.length; i++) {
-        txt += x[i].childNodes[0].nodeValue + "<br>";
+    txt = "<tr><th>Title</th><th>Artist</th><th>Country</th><th>Company</th><th>Price</th><th>Year</th></tr>";
+    x = xmlDoc.getElementsByTagName("CD");
+    let added = 0;
+    for (i = 0; i < x.length; i++) {
+        if (filter(x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue, x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue)) {
+            txt += "<tr>";
+            txt += "<td class='main-table-content'>" + x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue + "</td>";
+            txt += "<td class='main-table-content'>" + x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue + "</td>";
+            txt += "<td class='main-table-content'>" + x[i].getElementsByTagName("COUNTRY")[0].childNodes[0].nodeValue + "</td>";
+            txt += "<td class='main-table-content'>" + x[i].getElementsByTagName("COMPANY")[0].childNodes[0].nodeValue + "</td>";
+            txt += "<td class='main-table-content'>" + x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue + "</td>";
+            txt += "<td class='main-table-content'>" + x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue + "</td>";
+            txt += "</tr>";
+            added++;
+        }
     }
-    document.getElementById("test").innerHTML = txt;
+    if (added == 0) {
+        document.getElementById("test").innerHTML = "<tr><th>Nothing Found</th></tr>";
+    } else {
+        document.getElementById("test").innerHTML = txt;
+    }
 }
